ac8bd780f5a10e3df9cf94e13e6cc1e097818a22
Rahul Raveendran <rahu3r@gmail.com>
2020-08-19 18:20:46 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let name;
+</script>
+
+<style lang="scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+    p{
+      a{
+        color: green;
+      }
+    }
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Scss setup is based on
+    <a target="_blank" href="https://medium.com/@sean_27490/svelte-sapper-with-sass-271fff662da9">Medium</a>
+    article
+  </p>
+</main>
